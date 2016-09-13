d38a135098513268d7700d0b112bd7633beb77be
Steve Taylor <smtaylor88@hotmail.com>
2016-09-12 20:03:21 -0700

    Updated severly!

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 ﻿var app = require('express')()
+    , exprss = require('express')
     , server = require('http').Server(app)
+    , path = require('path')
+    , dotenv = require('dotenv')
+    , mongodb = require('mongodb')
     , io = require('socket.io').listen(server)
     , Twit = require('twit')
-    , path = require('path')
-    , mongodb = require('mongodb')
     , mongoose = require('mongoose');
 
-process.env.PORT = "8888";
-var port = process.env.PORT || 8888;
+// load all environment variables here (see file ./.env for values)
+dotenv.load();
+
+app.use(exprss.static('public'));
+app.use(exprss.static('views'));
+
+// by default, when running on Modulus, port 8080 is used
+var port = process.env.PORT || 8080;
+if (app.get("env") === 'development') {
+    port = 8888;
+}
 
 server.on('listening', function () {
     console.log('ok, server is running');
 });
 
 server.listen(port);
-console.log('Application Listening on port %s', process.env.PORT);
+console.log('Listening on port: %s', port);
+console.log('environment: %s', process.env.NODE_ENV);
 
-// Enter the following into command.com windows...to start mongodb
+// Enter the following into command.com windows...to start mongodb on the local server
 // mongod --dbpath="c:\users\steve\documents\visual studio 2015\projects\expressapp1\expressapp1\mongodb"
 // mongodb://<user>:<pass>@jello.modulusmongo.net:27017/anAhe2xu
 
 //mongoose.connect('mongodb://localhost/tweet', function (err) {
-    mongoose.connect('mongodb://CaptiveDBAdmin:Captive123@jello.modulusmongo.net:27017/anAhe2xu', function (err) {
+mongoose.connect('mongodb://CaptiveDBAdmin:Captive123@jello.modulusmongo.net:27017/anAhe2xu', function (err) {
     if (err) {
         console.log(err);
     } else {
@@ -30,33 +42,74 @@ console.log('Application Listening on port %s', process.env.PORT);
 });
 
 // routing
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get('/Index', function (req, res) {
+    if (app.get("env") === 'development') {
+        res.sendFile(__dirname + '/views/DevIndex.html');
+    } else {
+        res.sendFile(__dirname + '/views/Index.html');
+    }
+});
+
+app.get('/test', function (req, res) {
+    res.sendFile(__dirname + '/test.html');
+});
+
+app.get('/tweetable', function (req, res) {
+    res.sendFile(__dirname + '/views/tweetableTest.html');
 });
 
 //// view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
 
-var watchList = ['#trump'];
-var T = new Twit({
-    consumer_key: 'nHSgwpsVbY6mkYumHCjkWibgI',
-    consumer_secret: 'QAdrEScuGVkkDKn0OPEYcScRpz7X36xzzbaSFrjrd21sJoOxZX',
-    access_token: '1932331075-OivpoRGbgmoMMEh9Lvtg1x3mE74EJ5Gxxm4gTif',
-    access_token_secret: 'PpP5hauNUhyT3RuAM7q5baSNhMDdZ2xGXNtb1gVSEa58W'
-})
+// an array of search words
+var watchList = ['#none'];
 
-function logArrayElements(element, index, arry) {
-    console.log('Search Item ' + index + ': ' + element);
+// core program call here!!!
+streamTwitter();
+
+function streamTwitter() {
+    io.on('connection', function (socket) {
+        console.log('Connected %s', socket.client.id);
+        watchList.forEach(logArrayElements);
+
+        socket.on('startTwitter', function (data) {
+            watchList = [ data ];
+            StartTwitterStream(io);
+            var d = new Date();
+            console.log('Start Twitter Feed: ' + watchList + ' [' + d.toTimeString() + ']');
+        });
+
+        socket.on('stopTwitter', function () {
+            var d = new Date();
+            console.log('Stop Twitter Feed!' + ' [' + d.toTimeString() + ']');
+            stream.stop();
+        });
+
+        socket.on('restartTwitter', function () {
+            var d = new Date();
+            console.log('Restart Twitter Feed:' + watchList + ' [' + d.toTimeString() + ']');
+            stream.start();
+        });
+
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+        });
+    });
 }
 
-io.on('connection', function (socket) {
-    console.log('Connected');
-    watchList.forEach(logArrayElements);
+var T = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_SECRET
+})
+var stream;
 
-    // var stream = T.stream('statuses/filter', { locations: '-122.75,6.8,-121.75,37.8' }) // san francisco
-    // var stream = T.stream('statuses/filter', { locations: '-122.11,45.8,-121.75,46.8' }) // tacoma
-    var stream = T.stream('statuses/filter', { track: watchList })
+function StartTwitterStream(io) {
+    // stream = T.stream('statuses/filter', { locations: '-122.75,6.8,-121.75,37.8' }) // san francisco
+    // stream = T.stream('statuses/filter', { locations: '-122.574, 47.205, -122.444, 47.287' }) // tacoma
+    stream = T.stream('statuses/filter', { track: watchList })
 
     stream.on('tweet', function (tweet) {
         var media;
@@ -67,18 +120,10 @@ io.on('connection', function (socket) {
         }
 
         // send the tweet with images!!!
-        io.sockets.emit('stream', tweet.user.screen_name, tweet.text, tweet.user.profile_image_url, tweet.created_at, tweet.user.location, media);
+        io.emit('stream', tweet.user.screen_name, tweet.text, tweet.user.profile_image_url, tweet.created_at, tweet.user.location, media);
     });
+}
 
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-
-    //socket.on('reset stream', function (msg) {
-    //    console.log('Reset Request Received: ' + msg);
-    //});
-});
-
-console.log('Down to the bottom...');
-
-module.exports = app;
+function logArrayElements(element, index, arry) {
+    console.log('Search Item ' + index + ': ' + element);
+}
---
+++ setup.js
@@ -0,0 +1,4 @@
+﻿'use strict';
+var fs = require('fs');
+fs.createReadStream('.sample-env')
+    .pipe(fs.createWriteStream('.env'));
